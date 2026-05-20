e19f01253451cdebc1c61ccedebfb1f6c8725750
Radhakrishna Kodari <kodariradhakrishna6@gmail.com>
2026-05-20 21:12:15 +0530

    9:12

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
   const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ecommerce';
-  await mongoose.connect(uri);
+  await mongoose.connect(uri, { serverSelectionTimeoutMS: 8000 });
   console.log('MongoDB connected');
 }
 
